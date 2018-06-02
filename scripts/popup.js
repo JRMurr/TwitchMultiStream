b64ba57e1c87a557fd1c49d9e0dc9fe6de90d27e
John Murray <jmurray@immuta.com>
2018-06-02 15:53:50 -0400

    Editing multistream now works

--- scripts/popup.js
+++ scripts/popup.js
@@ -64,19 +64,29 @@
                 alert("Cant have more then 6 streams in a multistream, uncheck some streams");
                 return;
             }
-        }
+        };
+
+        function arrayUnique(array) {
+            var a = array.concat();
+            for (var i = 0; i < a.length; ++i) {
+                for (var j = i + 1; j < a.length; ++j) {
+                    if (a[i] === a[j])
+                        a.splice(j--, 1);
+                }
+            }
+            return a;
+        };
+
         var multiRes = event.data.multiRes;
         var includedStreams = event.data.streams.filter(function (stream) {
             var elm = $('#' + stream.streamName);
-            return $('#' + stream.streamName).prop("checked");
+            return elm && elm.prop("checked");
         });
 
-        // for (var i = streamsInMulti.length - 1; i >= 0; i--) {
-        //     multiStreamName = streamsInMulti[i];
-        //     if ($('#' + multiStreamName).prop("checked") == false) {
-        //         streamsInMulti.splice(i, 1); //,1 means remove only 1
-        //     }
-        // };
+        var includedMutlti = multiRes.streamsInMulti.filter(function (stream) {
+            var elm = $('#multi-' + stream);
+            return elm && elm.prop("checked");
+        });
 
         if ($("#closeTab").prop("checked")) {
             includedStreams.forEach(element => {
@@ -84,30 +94,15 @@
             });
         }
 
-
-        if (multiRes.isMulti)
-        {
-            //Checks to make sure updated multi is still less then 6 by combining current streams and new into one array
-            var allStreams = arrayUnique(streamsInMulti.concat(checkedStreams));
-            if(favStream){
-                allStreams = arrayUnique(allStreams.concat(favStream));
-            }
-            if (allStreams.length <= 6) {
-                var layoutNumber = 0;
-                multistreamLink = "http://multistre.am/";
-                for (var i = 0; i < allStreams.length; i++) {
-                    multistreamLink = multistreamLink + allStreams[i] + "/";
-                    layoutNumber += 3;
-                };
-                multistreamLink = multistreamLink + "/layout" + layoutNumber;
-                chrome.tabs.update(currentTab.id, {
-                    url: multistreamLink
-                });
-            } else if (checkedStreams.length > 6)
-                alert("Cant have more then 6 streams in a multistream, uncheck some streams");
+        var streamNames = includedStreams.map(elm => elm.streamName);
+        if (multiRes.isMulti) {
+            var allStreams = arrayUnique(includedMutlti.concat(streamNames));
+            var url = getMultiUrl(allStreams)
+            chrome.tabs.update(multiRes.currentTab.id, {
+                url: url
+            });
         }
         else {
-            var streamNames = includedStreams.map(elm => elm.streamName);
             var url = getMultiUrl(streamNames)
             console.log(`url: ${url}`);
             if (url) {
@@ -118,60 +113,25 @@
         }
     }
 
-    //used to combine 2 arrays and remove duplicates
-    function arrayUnique(array) {
-        var a = array.concat();
-        for (var i = 0; i < a.length; ++i) {
-            for (var j = i + 1; j < a.length; ++j) {
-                if (a[i] === a[j])
-                    a.splice(j--, 1);
-            }
-        }
-        return a;
-    };
-
     //checks if current tab is a multistream and updates iscurrntmulti to true if is multi and updtes current tab to current tab if multi
     function checkIfMulti() {
         return new Promise(function(resolve, reject){
             chrome.tabs.getSelected(null, function(currTab) {
                 var parser = document.createElement('a');
                 parser.href = currTab.url;
-                resolve({isMulti: parser.hostname === 'multistre.am', currentTab: currTab});
+                var ret = {isMulti: parser.hostname === 'multistre.am', currentTab: currTab, streamsInMulti:[]};
+                if (ret.isMulti) {
+                    var urlPath = parser.pathname.substring(1);
+                    ret.streamsInMulti = urlPath.split('/').filter(function (elm) {
+                        return !(elm === '' || elm.startsWith('layout'));
+                    });
+                }
+                resolve(ret);
             });
         });
     }
 
-    function getStreamsinMulti() {
-        var temp = currentTab.url;
-        //clean up url
-        temp = temp.replace("http://multistre.am/", '');
-        var index = temp.indexOf("layout");
-        //get num of streams
-        var layout = temp.substring(index);
-        layout = layout.replace("layout", '');
-        layout = layout.replace("/", '');
-        var layoutNumber = parseInt(layout);
-        var numStreams = 1;
-        while (layoutNumber != 0) {
-            numStreams++;
-            layoutNumber -= 3;
-        }
-        //adds streams to streamsInMulti
-        temp = temp.substring(0, index);
-
-        while (temp.indexOf("/") != -1) //while there are still streams since each stream name should have a '/' after it
-        {
-            stream = temp.substring(0, temp.indexOf("/")); //substring from beginning to end of the streamname (should just be stream name nothing else)
-            stream = stream.trim();
-            streamsInMulti.push(stream);
-            temp = temp.substring(temp.indexOf("/") + 1); //set temp to string without the stream just added the array
-        }
-    }
-
-
-
     //updates streams and lists them in popup.html
-    //Ty to k9 for the great grammar tips
     function updatePopUp(multiRes, streams) {
         var popUpHtml = "";
         function addButton(buttonId, buttonText) {
@@ -186,43 +146,27 @@
             return `<${elementType}>${text}</${elementType}>`;
         }
         if (multiRes.isMulti) {
-            popUpHtml += addElement('h4', 'Streams In Multi Stream');
-            for (var i = 0; i < streamsInMulti.length; i++) {
-                var stream = streamsInMulti[i];
-                popUpHtml += addButton(stream);
-            };
+            popUpHtml += addElement('h6', 'Streams In Multi Stream');
+            var tmp = multiRes.streamsInMulti.map(stream => addButton(`multi-${stream}`, stream)).join('');
+            popUpHtml += tmp;
 
-            if (checkedStreams.length <= 6 && checkedStreams.length > 0) {
-                popUpHtml += addElement('h4', 'Opened Stream Tabs');
-                var numNewStreams = 0;
-                for (var checkedStreamName in checkedStreams) {
-                    var streamInMulti = false;
-                    for (var stream in streamsInMulti) {
-                        if (checkedStreamName === stream) {
-                            streamInMulti = true;
-                            break;
-                        }
-                    };
-                    if (!streamInMulti) {
-                        popUpHtml += addButton(checkedStreamName);
-                        numNewStreams++;
+            if (streams.length <= 6 && streams.length > 0) {
+                var newPopUp = '';
+                streams.forEach(function (stream) {
+                    if (!multiRes.streamsInMulti.includes(stream.streamName)) {
+                        newPopUp += addButton(stream.streamName);
                     }
-                };
-                if (numNewStreams > 0) {
-                    popUpHtml += addButton('closeTab', 'Close streams?');
-                } else {
-                    popUpHtml = popUpHtml.substring(0, popUpHtml.length - 33);
-                }
+                });
+                if (newPopUp != '') {
+                    popUpHtml = popUpHtml + addElement('h6', 'Opened Stream Tabs') + newPopUp + addButton('closeTab', 'Close streams?');
+                } 
             }
         } else {
             if (streams.length <= 0)
-                popUpHtml += addElement('h4', 'No Stream tabs Opened');
+                popUpHtml += addElement('h6', 'No Stream tabs Opened');
             else if (streams.length <= 6) {
-                popUpHtml += addElement('h5', 'Opened Stream Tabs');
-                // for (var i = 0; i < checkedStreams.length; i++) {
-                //     popUpHtml += addButton(checkedStreams[i])
-                // };
-                popUpHtml += streams.map(stream => addButton(getStreamName(stream.url))).join('')
+                popUpHtml += addElement('h6', 'Opened Stream Tabs');
+                popUpHtml += streams.map(stream => addButton(stream.streamName)).join('');
                 popUpHtml += addButton('closeTab', 'Close streams?');
             }
         }
